2a30824477362607e61273206c70b85f9e5c3ee1
andrea0727 <andrea0727.m@gmail.com>
2020-09-17 08:35:02 -0500

    first changes

---
+++ src/test/java/Version1spring/springboot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package Version1spring.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
